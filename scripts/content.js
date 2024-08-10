f8d76b3ab3668860a49400c75332077a4a286bc2
michael-jj-dev <michael.jj.dev@gmail.com>
2024-08-10 15:24:51 -0700

    Add Mutation Observer for Job Listings (#1)
    * Added MutationObserver to detect changes in listings
    
    * Modify MutationObserver to account for target html tag type

--- scripts/content.js
+++ scripts/content.js
@@ -11,3 +11,29 @@ chrome.runtime.sendMessage(
     );
   }
 );
+
+function listingMutated() {
+  console.log('mutation observed');
+}
+
+function onMutation(mutationsList, observer) {
+  mutationsList.forEach((mutation) => {
+    const isChildListMutation = mutation.type === 'childList';
+    const hasAddedNodes = mutation.addedNodes.length > 0;
+    const isRelevantTarget =
+      mutation.target.nodeName === 'LI' || mutation.target.nodeName === 'DIV';
+
+    if (!isChildListMutation || !hasAddedNodes || !isRelevantTarget) return;
+
+    mutation.addedNodes.forEach((node) => {
+      const isRelevantNode = node.nodeName === 'LI' || node.nodeName === 'DIV';
+
+      if (!isRelevantNode) return;
+
+      listingMutated();
+    });
+  });
+}
+
+const observer = new MutationObserver(onMutation);
+observer.observe(document.body, { childList: true, subtree: true });
