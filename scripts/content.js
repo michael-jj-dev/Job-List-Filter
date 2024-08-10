0948f322a619cda1dae48bb6965f732da9508f4a
michael-jj-dev <michael.jj.dev@gmail.com>
2024-08-10 14:59:16 -0700

    Added MutationObserver to detect changes in listings

--- scripts/content.js
+++ scripts/content.js
@@ -11,3 +11,27 @@ chrome.runtime.sendMessage(
     );
   }
 );
+
+function listingMutated() {
+  console.log('mutation observed');
+}
+
+function onMutation(mutationsList, observer) {
+  mutationsList.forEach((mutation) => {
+    const isChildListMutation = mutation.type === 'childList';
+    const hasAddedNodes = mutation.addedNodes.length > 0;
+
+    if (!isChildListMutation || !hasAddedNodes) return;
+
+    mutation.addedNodes.forEach((node) => {
+      const isRelevantNode = node.nodeName === 'LI' || node.nodeName === 'DIV';
+
+      if (!isRelevantNode) return;
+
+      listingMutated();
+    });
+  });
+}
+
+const observer = new MutationObserver(onMutation);
+observer.observe(document.body, { childList: true, subtree: true });
