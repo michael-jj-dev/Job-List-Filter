e37fa73de5535330d8788e64ed3743c045332bd5
michael-jj-dev <michael.jj.dev@gmail.com>
2024-08-24 12:16:51 -0700

    Establish popup.js scripts and communication between popup.js and content.js via the SW (#3)

--- scripts/content.js
+++ scripts/content.js
@@ -1,16 +1,51 @@
 var port = chrome.runtime.connect({ name: 'knockknock' });
 
-chrome.runtime.sendMessage(
-  {
-    message: 'Content script injected'
-  },
-  function (response) {
-    console.log(
-      'Message sent to background script or service worker:',
-      response
-    );
+let targetEnabled = true;
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === 'toggleTargetter') {
+    console.log('Command received from popup via background.js');
+    targetEnabled = true;
   }
+});
+
+const htmlElement = document.querySelector('html'); // Or document.querySelector('html');
+
+htmlElement.addEventListener(
+  'click',
+  (event) => {
+    if (targetEnabled) {
+      const listing = findNearestListing(event.target);
+      const listingsAttributes = getElementAttributes(listing);
+
+      targetEnabled = false;
+    }
+  },
+  true
 );
+
+function onMutation(mutationsList, observer) {
+  mutationsList.forEach((mutation) => {
+    const isChildListMutation = mutation.type === 'childList';
+    const hasAddedNodes = mutation.addedNodes.length > 0;
+    const isRelevantTarget =
+      mutation.target.nodeName === 'LI' || mutation.target.nodeName === 'DIV';
+
+    if (!isChildListMutation || !hasAddedNodes || !isRelevantTarget) return;
+
+    mutation.addedNodes.forEach((node) => {
+      const isRelevantNode = node.nodeName === 'LI' || node.nodeName === 'DIV';
+
+      if (!isRelevantNode) return;
+
+      listingMutated();
+    });
+  });
+}
+
+function listingMutated() {
+  console.log('listingMutated');
+}
 
 function findNearestListing(element) {
   var currentElement = element.parentElement;
@@ -42,49 +77,13 @@ function findNearestListing(element) {
 function getElementAttributes(element) {
   let attributesObj = {};
 
-  Array.from(element.attributes).forEach((attr) => {
-    attributesObj[attr.name] = attr.value;
-  });
+  if (element.attributes !== null) {
+    Array.from(element.attributes).forEach((attr) => {
+      attributesObj[attr.name] = attr.value;
+    });
+  }
 
   return attributesObj;
-}
-
-const htmlElement = document.querySelector('html'); // Or document.querySelector('html');
-
-htmlElement.addEventListener(
-  'click',
-  (event) => {
-    const listing = findNearestListing(event.target);
-
-    let listingsAttributes = getElementAttributes(listing);
-
-    console.log(listing);
-    console.log('listingsAttributes:', listingsAttributes);
-  },
-  true
-);
-
-function listingMutated() {
-  console.log('mutation observed');
-}
-
-function onMutation(mutationsList, observer) {
-  mutationsList.forEach((mutation) => {
-    const isChildListMutation = mutation.type === 'childList';
-    const hasAddedNodes = mutation.addedNodes.length > 0;
-    const isRelevantTarget =
-      mutation.target.nodeName === 'LI' || mutation.target.nodeName === 'DIV';
-
-    if (!isChildListMutation || !hasAddedNodes || !isRelevantTarget) return;
-
-    mutation.addedNodes.forEach((node) => {
-      const isRelevantNode = node.nodeName === 'LI' || node.nodeName === 'DIV';
-
-      if (!isRelevantNode) return;
-
-      listingMutated();
-    });
-  });
 }
 
 const observer = new MutationObserver(onMutation);
--- scripts/service_worker.js
+++ scripts/service_worker.js
@@ -1,5 +1,19 @@
-// Listener for messages from content scripts
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  console.log(message);
-  sendResponse({ received: true });
+chrome.runtime.onConnect.addListener(function (port) {
+  //long term connection
+  console.log('Connected to:', port.name);
+
+  if (port.name === 'knockknock') {
+    port.onMessage.addListener(function (msg) {
+      console.log('Message received:', msg);
+    });
+  }
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  //one-time connection
+  if (request.action === 'toggleTargetter') {
+    chrome.tabs.sendMessage(request.tabId, {
+      action: request.action
+    });
+  }
 });
